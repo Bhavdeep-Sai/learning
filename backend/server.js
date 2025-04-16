60015e49d48bdefce601e007ec36e365b2455adc
Bhavdeep-Sai <bhavdeepsai@gmail.com>
2025-04-16 17:25:10 +0530

    final change

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 
 
 
-mongoose.connect(process.env.MongoDB_URL)
+mongoose.connect(process.env.MONGO_URL)
 .then(()=>{console.log("MongoDB connected")})
 .catch((err)=>{console.log("MongoDB connection Failed", err)})
 
